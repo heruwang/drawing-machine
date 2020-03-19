488cc08a2c746448516b3b2cd202a86f6cdd6d37
Heru Wang <heru.wang@sjsu.edu>
2020-03-18 17:53:56 -0700

    drawing machine

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let array=[];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+    background(220);
+    strokeWeight(15);
 }
 
 function draw() {
-  background(220);
+if (mouseIsPressed){
+
+   line(mouseX, mouseY, pmouseX, pmouseY);
+   array.push([mouseX, mouseY]);
+ }
+}
+function keyTyped(){
+
+  if (key ==='s'){
+    //save the image
+    saveCanvas('fileName', 'png');
+  }else if(key === 'd'){
+     //display image
+     console.log(array);
+  }
+  return false;
 }
