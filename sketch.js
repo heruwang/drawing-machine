51c912a95cb403c0c6e8facdf37313a29e517a3c
Heru Wang <heru.wang@sjsu.edu>
2020-03-26 18:07:22 -0700

    done!
    done

--- sketch.js
+++ sketch.js
@@ -1,50 +1,74 @@
-let array=[];
+let array = [];
 let backgroundColor = 200;
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(backgroundColor);
-    strokeWeight(5);
-    noFill();
+  background(backgroundColor);
+  strokeWeight(5);
+  noFill();
+  drawGrid();
 
 }
 
 function draw() {
-if (mouseIsPressed){
-   backgroundColor-=5;
-   background(backgroundColor);
-   //line(mouseX, mouseY, pmouseX, pmouseY)；
-   array.push([mouseX, mouseY]);
 
-   beginShape();
-   for(let i = 0; i < array.length-1; i++){
-     //line(array[i][0], array[i][1], array[1+i][0], array[1+i][1]);
-     curveVertex(array[i][0], array[i][1]);
-   }
-   endShape();
- }
+  if (mouseIsPressed) {
+    backgroundColor -= 5;
+    background(backgroundColor);
+    //line(mouseX, mouseY, pmouseX, pmouseY)；
+    array.push([mouseX, mouseY]);
+
+    beginShape();
+    for (let i = 0; i < array.length - 1; i++) {
+      //line(array[i][0], array[i][1], array[1+i][0], array[1+i][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+  }
 }
-function keyTyped(){
 
-  if (key ==='s'){
+function keyTyped() {
+
+  if (key === 's') {
     //save the image
     saveCanvas('fileName', 'png');
-  }else if(key === 'd'){
-     //display image
-     background(255);
-     beginShape();
-     for(let i = 0; i < array.length-1; i++){
-       //line(array[i][0], array[i][1], array[1+i][0], array[1+i][1]);
-       curveVertex(array[i][0], array[i][1]);
-     }
-     endShape() ;
-  }else if(key === 'c'){
+  } else if (key === 'd') {
+    //display image
+    background(255);
+    beginShape();
+    for (let i = 0; i < array.length - 1; i++) {
+      //line(array[i][0], array[i][1], array[1+i][0], array[1+i][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+  } else if (key === 'c') {
     clear();
   }
   return false;
 }
 
-function mousePressed(){
-  array=[];
+function drawGrid() {
+  fillColor = 255;
+  numCells = 30;
+  strokeWeight(0);
+  for (let i = 0; i <= width; i += width / numCells) {
+    for (let j = 0; j <= height; j += height / numCells) {
+      if (fillColor === 255) {
+        fillColor = 200;
+      } else {
+        fillColor = 255;
+      }
+      fill(fillColor);
+      rect(i, j, width / numCells, height / numCells);
+    }
+  }
+  strokeWeight(5);
+}
+
+function mousePressed() {
+  array = [];
   backgroundColor = 255;
 
 }
